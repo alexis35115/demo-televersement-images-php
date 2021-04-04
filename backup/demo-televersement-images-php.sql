750ab25c7a1c0f45d9ff6756f2b1f26a2d7ec926
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-04-04 19:17:42 -0400

    ajouter deux vaches, les images et mettre à jour le backup de la bd

--- backup/demo-televersement-images-php.sql
+++ backup/demo-televersement-images-php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : Dim 04 avr. 2021 à 19:11
+-- Généré le : Dim 04 avr. 2021 à 23:17
 -- Version du serveur :  8.0.22
 -- Version de PHP : 7.4.12
 
@@ -38,6 +38,14 @@ CREATE TABLE `vache` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Déchargement des données de la table `vache`
+--
+
+INSERT INTO `vache` (`id_vache`, `nom`, `date_creation`, `nom_image`) VALUES
+(3, 'Grosse toutoune', '2021-04-04 19:14:39', '167056921_476766860185173_3669610519261285888_n.jpg'),
+(4, 'Toupette', '2021-04-04 19:15:13', '167417644_283398773396751_564948521875749251_n.jpg');
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -55,7 +63,7 @@ ALTER TABLE `vache`
 -- AUTO_INCREMENT pour la table `vache`
 --
 ALTER TABLE `vache`
-  MODIFY `id_vache` int NOT NULL AUTO_INCREMENT COMMENT 'Clé primaire de la table vache';
+  MODIFY `id_vache` int NOT NULL AUTO_INCREMENT COMMENT 'Clé primaire de la table vache', AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
